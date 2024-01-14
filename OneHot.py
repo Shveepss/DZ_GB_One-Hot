d2a0be20cc02d223c695cef2f8c78861c36667d6
Yury <shveepss@mail.ru>
2024-01-14 16:00:41 +0300

    delete dot

--- OneHot.py
+++ OneHot.py
@@ -3,19 +3,19 @@ from pandas import DataFrame
 
 
 def one_hot_encode(df, column):
-    # Получаем уникальные значения столбца.
+    # Получаем уникальные значения столбца
     categories = df[column].unique()
 
-    # Создаем для каждого значения столбца новый столбец.
+    # Создаем для каждого значения столбца новый столбец
     for category in categories:
         df[category] = 0
 
     # В каждом столбце устанавливаем значение 1 для записи, в которой
-    # значение столбца column совпадает с названием столбца.
+    # значение столбца column совпадает с названием столбца
     for i in range(len(df)):
         df.loc[i, df.loc[i, column]] = 1
 
-    # Удаляем исходный столбец.
+    # Удаляем исходный столбец
     df.drop(column, axis=1, inplace=True)
 
     return df
